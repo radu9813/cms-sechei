e7f1050df83565aa8b993a8b2e60c9be492de888
radu9813 <radu9813@gmail.com>
2021-09-05 23:58:57 +0300

    Function to make my table get data from local storage + deleteRow function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
-function deleteRow(){
-    document.getElementById("myRow").delete();
+function deleteRow(id){
+    document.getElementById(id.toString()).remove();
 };
 
 function submitData(){
@@ -17,5 +17,32 @@ function submitData(){
     };
     users.push(user);
     window.localStorage.setItem("users",JSON.stringify(users));
-    console.log(JSON.stringify(users));
+    console.log(users);
+    fetchTable();
 }
+
+
+
+function fetchTable() {
+    var loadedData = JSON.parse(window.localStorage.getItem("users"));
+    var loadedTable = document.getElementById("usersTable");
+    console.log(loadedData);
+    for (var entry in loadedData) {
+        var user = loadedData[entry];
+      console.log(user.nume);
+      var row = loadedTable.insertRow();
+      row.id = user.id;
+      var numeCell = row.insertCell(0);
+      numeCell.innerHTML = user.nume;
+      var prenumeCell = row.insertCell(1);
+      prenumeCell.innerHTML = user.prenume;
+      var emailCell = row.insertCell(2);
+      emailCell.innerHTML = user.email;
+      var sexCell = row.insertCell(3);
+      sexCell.innerHTML = user.sex;
+      var birthDateCell = row.insertCell(4);
+      birthDateCell.innerHTML = user.birthDate;
+      var actionsCell = row.insertCell(5);
+      actionsCell.innerHTML = `<button onClick="deleteRow(${user.id})">Delete</button>`;
+    }
+  }
