c84f5e277ca03f806b13fdccb2455df1501ba925
radu9813 <radu9813@gmail.com>
2021-09-05 23:18:36 +0300

    Making the buton to save data using local storage

--- index.js
+++ index.js
@@ -5,5 +5,17 @@ function deleteRow(){
 };
 
 function submitData(){
-    console.log(Date.now());
+    var users = JSON.parse(window.localStorage.getItem("users") || "[]");
+    console.log("# of users: " + users.length);
+    var user = {
+        id: Date.now(),
+        nume: document.getElementById("nume").value,
+        prenume: document.getElementById("prenume").value,
+        email: document.getElementById("email").value,
+        sex: document.getElementById("sex").value,
+        birthDate: document.getElementById("birthdate").value
+    };
+    users.push(user);
+    window.localStorage.setItem("users",JSON.stringify(users));
+    console.log(JSON.stringify(users));
 }
